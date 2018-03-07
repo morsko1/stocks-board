b0def03df536d5d2a6bcf88e169ba626a7102871
morsko1 <morsko1@yandex.ru>
2018-03-07 13:58:45 +0300

    added: keys, loader

--- src/app.js
+++ src/app.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import {
     Route,
-    Link,
 } from 'react-router-dom';
 import Home from './containers/home';
 import About from  './containers/about';
 
 const App = () => (
     <div>
-        {/*<header>
-            <Link to='/'>Home</Link>
-            <Link to='/about-us'>About</Link>
-        </header>*/}
         <main>
             <Route exact path='/' component={Home} />
             <Route exact path='/about-us' component={About} />
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,17 +3,17 @@ import './index.css';
 
 const getRow = (item) => {
     return (
-        <tr>
-            <td>{item.ticker}</td>
-            <td>{item.shortName}</td>
-            <td>{item.open}</td>
-            <td>{item.last}</td>
-            <td>{item.volumeToday}</td>
+        <tr key={`row_${item.ticker}`}>
+            <td key={`col_ticker${item.ticker}`}>{item.ticker}</td>
+            <td key={`col_name${item.ticker}`}>{item.shortName}</td>
+            <td key={`col_open${item.ticker}`}>{item.open}</td>
+            <td key={`col_last${item.ticker}`}>{item.last}</td>
+            <td key={`col_volume${item.ticker}`}>{(item.volumeToday/1000000).toFixed(2)}</td>
             {/*
                 capitalization in $ billions.
                 to fix: get correct $ price
             */}
-            <td>{parseFloat(item.capitalization/(1000000000*57)).toFixed(3)}</td>
+            <td key={`col_cap${item.ticker}`}>{parseFloat(item.capitalization/(1000000000*57)).toFixed(3)}</td>
         </tr>
     );
 }
@@ -23,23 +23,23 @@ const HomeView = props => (
         <h3>Московская Биржа</h3>
         {
             props.stocksFetching && !props.stocks.data.length ?
-                <div style={{textAlign: 'center'}}>
-                    {'Загрузка...'}
-                </div> :
+                <div className={'loader'} /> :
                 <div>
                     <table>
-                        <tr>
-                            <th>Тикер</th>
-                            <th>{'Наименование'}</th>
-                            <th>{'Цена,\nоткр'}</th>
-                            <th>{'Цена,\nпосл'}</th>
-                            <th>{'Объем'}</th>
-                            <th>{'Капитализация,\nмлрд'}</th>
-                        </tr>
-                        {
-                            // first 30 stocks
-                            props.stocks.data.slice(0, 30).map(item => getRow(item))
-                        }
+                        <tbody>
+                            <tr>
+                                <th key={'head_ticker'}>Тикер</th>
+                                <th key={'head_name'}>{'Наименование'}</th>
+                                <th key={'head_open'}>{'Цена,\nоткр'}</th>
+                                <th key={'head_last'}>{'Цена,\nпосл'}</th>
+                                <th key={'head_volume'}>{'Объем,\nмлн р'}</th>
+                                <th key={'head_cap'}>{'Капитализация,\nмлрд $'}</th>
+                            </tr>
+                            {
+                                // first 30 stocks
+                                props.stocks.data.slice(0, 30).map(item => getRow(item))
+                            }
+                        </tbody>
                     </table>
                 </div>
         }
