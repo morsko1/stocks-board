0c6c51a785cf6004cbcfd98cad98f8ed10ed771b
Alexander <morsko1@yandex.ru>
2018-10-08 20:31:12 +0300

    added currencies component

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './index.css';
 import HeaderView from './parts/header.js';
+import CurrenciesView from './parts/currencies.js';
 
 const HomeView = props => {
     return (
         <div className={'home-container'}>
             <HeaderView />
+            <CurrenciesView currencies={props.currencies} />
         </div>
     );
 }
