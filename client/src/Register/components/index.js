1f48ba9c83d1801870cfa38ddfe431c83fda2d4f
Alexander <morsko1@yandex.ru>
2018-10-11 22:49:04 +0300

    added register form

--- client/src/Register/components/index.js
+++ client/src/Register/components/index.js
@@ -4,9 +4,47 @@ import './index.css';
 const RegisterView = props => {
     return (
         <div className={'register-container'}>
-            {props.testData}
-            <br/>
-            <button onClick={() => props.test()}>test</button>
+            <div className={'register-container__inner'}>
+                <div className={'register-container__title'}>Register</div>
+                <form className={'register-container__form'} onSubmit={(e) => {e.preventDefault()}}>
+                    <div className={'register-container__username'}>
+                        <label htmlFor={'register-container__username-input'}>username</label>
+                        <input
+                            id={'register-container__username-input'}
+                            type={'text'}
+                        />
+                    </div>
+                    <div className={'register-container__email'}>
+                        <label htmlFor={'register-container__email-input'}>email</label>
+                        <input
+                            id={'register-container__email-input'}
+                            type={'text'}
+                        />
+                    </div>
+                    <div className={'register-container__password'}>
+                        <label htmlFor={'register-container__password-input'}>password</label>
+                        <input
+                            id={'register-container__password-input'}
+                            type={'password'}
+                        />
+                    </div>
+                    <div className={'register-container__password-confirm'}>
+                        <label htmlFor={'register-container__password-confirm-input'}>confirm password</label>
+                        <input
+                            id={'register-container__password-confirm-input'}
+                            type={'password'}
+                        />
+                    </div>
+                    <div className={'register-container__submit-button-wrapper'}>
+                        <button
+                            type={'submit'}
+                            className={'register-container__submit-button'}
+                        >
+                            register
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
