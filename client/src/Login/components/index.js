a3c39a8cec10ff3523fb776047fcb7dfa1745151
morsko1 <morsko1@yandex.ru>
2018-11-07 23:59:50 +0300

    refactored Login and Register components, added errors

--- client/src/Login/components/index.js
+++ client/src/Login/components/index.js
@@ -1,68 +1,108 @@
 import React from 'react';
 import './index.scss';
 
+const getUserNameInput = (props) => {
+    return (
+        <div className={'login-container__username'}>
+            <label htmlFor={'login-container__username-input'}>username</label>
+            <input
+                id={'login-container__username-input'}
+                type={'text'}
+                data-field={'username'}
+                value={props.input.username}
+                onChange={(e) => {props.handleInput(e.target.dataset.field, e.target.value)}}
+                required
+            />
+        </div>
+    );
+}
+
+const getPasswordInput = (props) => {
+    return (
+        <div className={'login-container__password'}>
+            <label htmlFor={'login-container__password-input'}>password</label>
+            <input
+                id={'login-container__password-input'}
+                type={'password'}
+                data-field={'password'}
+                value={props.input.password}
+                onChange={(e) => {props.handleInput(e.target.dataset.field, e.target.value)}}
+                required
+            />
+        </div>
+    );
+}
+const getNavigationLinks = (props) => {
+    return (
+        <div>
+            <div className={'login-container__link-to-register-wrapper'}>
+                or
+                <span
+                    className={'login-container__link-to-register'}
+                    onClick={()=>{props.goToRegisterPage()}}
+                >
+                    register
+                </span>
+            </div>
+            <div className={'login-container__link-to-home-wrapper'}>
+                <span
+                    className={'login-container__link-to-home'}
+                    onClick={()=>{props.goToHomePage()}}
+                >
+                    home
+                </span>
+            </div>
+        </div>
+    );
+}
+
+const getLoginButton = (props) => {
+    return (
+        <div className={'login-container__submit-button-wrapper'}>
+            <button
+                type={'submit'}
+                className={'login-container__submit-button'}
+            >
+                login
+            </button>
+        </div>
+    );
+}
+
+const getLoginContainer = (props) => {
+    return (
+        <div className={'login-container__inner'}>
+            <div className={'login-container__title'}>Login</div>
+            <form
+                id={'login-container__form'}
+                className={'login-container__form'}
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    props.login();
+                }}
+            >
+                {getUserNameInput(props)}
+                {getPasswordInput(props)}
+                {getLoginButton(props)}
+                {getNavigationLinks(props)}
+            </form>
+        </div>
+    );
+}
+
 const LoginView = props => {
     return (
         <div className={'login-container'}>
-            <div className={'login-container__inner'}>
-                <div className={'login-container__title'}>Login</div>
-                <form
-                    id={'login-container__form'}
-                    className={'login-container__form'}
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        props.login();
-                    }}
-                >
-                    <div className={'login-container__username'}>
-                        <label htmlFor={'login-container__username-input'}>username</label>
-                        <input
-                            id={'login-container__username-input'}
-                            type={'text'}
-                            data-field={'username'}
-                            value={props.input.username}
-                            onChange={(e) => {props.handleInput(e.target.dataset.field, e.target.value)}}
-                            required
-                        />
-                    </div>
-                    <div className={'login-container__password'}>
-                        <label htmlFor={'login-container__password-input'}>password</label>
-                        <input
-                            id={'login-container__password-input'}
-                            type={'password'}
-                            data-field={'password'}
-                            value={props.input.password}
-                            onChange={(e) => {props.handleInput(e.target.dataset.field, e.target.value)}}
-                            required
-                        />
-                    </div>
-                    <div className={'login-container__submit-button-wrapper'}>
-                        <button
-                            type={'submit'}
-                            className={'login-container__submit-button'}
-                        >
-                            login
-                        </button>
-                    </div>
-                    <div className={'login-container__link-to-register-wrapper'}>
-                        or
-                        <span
-                            className={'login-container__link-to-register'}
-                            onClick={()=>{props.goToRegisterPage()}}
-                        >
-                            register
-                        </span>
-                    </div>
-                    <div className={'login-container__link-to-home-wrapper'}>
-                        <span
-                            className={'login-container__link-to-home'}
-                            onClick={()=>{props.goToHomePage()}}
-                        >
-                            home
-                        </span>
-                    </div>
-                </form>
-            </div>
+        {
+            props.loginError ?
+            <div className="login-container__error">{props.loginError.error}</div> :
+            null
+        }
+        {
+            props.isFetching ?
+                <div className="login-container__loader" /> :
+                getLoginContainer(props)
+        }
         </div>
     );
 }
--- client/src/Register/containers/index.js
+++ client/src/Register/containers/index.js
@@ -9,8 +9,8 @@ import * as navigation from '~/common/navigation.js';
 
 class Register extends Component {
 
-    componentDidMount() {
-        console.log('Register did mount');
+    componentWillUnmount() {
+       this.props.resetError();
     }
 
     render() {
@@ -18,6 +18,8 @@ class Register extends Component {
             <RegisterView
                 register={this.props.register}
                 input={this.props.input}
+                isFetching={this.props.isFetching}
+                registerError={this.props.registerError}
                 handleInput={this.props.handleInput}
                 goToLoginPage={this.props.goToLoginPage}
                 goToHomePage={this.props.goToHomePage}
@@ -27,13 +29,16 @@ class Register extends Component {
 }
 
 const mapStateToProps = state => ({
-    input: state.register.input
+    input: state.register.input,
+    isFetching: state.register.isFetching,
+    registerError: state.register.registerError
 });
 
 const mapDispatchToProps = dispatch =>
     bindActionCreators({
         register: () => thunkRegister.register(),
         handleInput: (field, text) => actionsRegister.handleInput(field, text),
+        resetError: () => actionsRegister.resetError(),
         goToLoginPage: navigation.goToLoginPage,
         goToHomePage: navigation.goToHomePage
     },
